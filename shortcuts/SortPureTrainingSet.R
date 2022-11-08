026fbe0261826ec497732ae44bf982ca59fd05ba
YvesBas <yves.bas@gmail.com>
2022-11-08 05:42:22 +0100

    right folder name

--- shortcuts/SortPureTrainingSet.R
+++ shortcuts/SortPureTrainingSet.R
@@ -1,7 +1,7 @@
 TrainingSetDir=choose.dir()
+SourceDir=choose.dir()
 
-
-SpecDir=paste0(TrainingSetDir,"/Spectrograms")
+SpecDir=paste0(SourceDir,"/oiseaux1")
 
 dir.create(SpecDir)
 
@@ -13,6 +13,14 @@ SpecInfos=tstrsplit(Spectrograms,split="/")
 Species=SpecInfos[[length(SpecInfos)-2]]
 print(table(Species))
 
-SortedSpectrograms=paste0(SpecDir,"/",Species,"--",Species,"--",basename(Spectrograms))
+
+SortedSpectrograms=paste0(SpecDir,"/",Species,"/",Species,"--",Species,"--",basename(Spectrograms))
+
+DirsToCreate=unique(dirname(SortedSpectrograms))
+
+for (i in 1:length(DirsToCreate)){
+  dir.create(DirsToCreate[i])
+  
+}
 
 file.copy(from=Spectrograms,to=SortedSpectrograms)
